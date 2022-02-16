1f02350ce8458ea953518b1d76a9b9be508e4e6b
Zaza Sichinava <zazasichina@gmail.com>
2022-02-16 19:38:44 +0400

    Solution to leetcode problem 2170. Minimum Operations to Make the Array Alternating

---
+++ 2170. Minimum Operations to Make the Array Alternating/leetcode_2170.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+        length = len(nums)
+        minSteps = math.inf
+
+        for evenKey, evenVal in Counter(nums[::2]).most_common(2):
+            for oddKey, oddVal in Counter(nums[1::2]).most_common(2):
+                steps = length - evenVal if evenKey == oddKey else length - evenVal - oddVal
+                minSteps = steps if steps < minSteps else minSteps
+        return minSteps
