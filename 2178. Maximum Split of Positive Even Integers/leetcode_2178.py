a44a6c02ce1a211fe8f2989c4cff6d73965f8d3e
Zaza Sichinava <zazasichina@gmail.com>
2022-02-21 17:53:01 +0400

    Solution to 2178. Maximum Split of Positive Even Integers

---
+++ 2178. Maximum Split of Positive Even Integers/leetcode_2178.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumEvenSplit(self, finalSum: int) -> List[int]:
+        if finalSum%2==1:
+            return []
+        else:
+            ans = []
+            i=0
+            while finalSum:
+                if 2*i+6 > finalSum:
+                  ans.append(finalSum)
+                  break
+                else:
+                  i+=2
+                  ans.append(i)
+                  finalSum-=i
+        return ans
