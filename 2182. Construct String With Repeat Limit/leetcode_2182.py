3d26e823a6baa95241a947a594bd32f51d0a0ea0
Zaza Sichinava <zazasichina@gmail.com>
2022-02-21 14:15:26 +0400

    Solution to 2182. Construct String With Repeat Limit

---
+++ 2182. Construct String With Repeat Limit/leetcode_2182.py
@@ -0,0 +1,22 @@
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        lst = sorted(list(s), reverse=True)
+        # print(lst)
+        cnt = 1
+        j=0
+        for i in range(1, len(lst)):
+            if lst[i-1] == lst[i]:
+                cnt+=1
+                if cnt > repeatLimit:
+                    j=max(j, i+1)
+                    while j<len(lst):
+                        if lst[j] != lst[i]:
+                            lst[j], lst[i] = lst[i], lst[j]
+                            cnt=1
+                            break
+                        j+=1
+                    if j>=len(lst):
+                        return ''.join(lst[:i])
+            else:
+              cnt = 1
+        return ''.join(lst)
