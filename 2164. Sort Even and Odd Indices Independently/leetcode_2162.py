7ad68733a95370fadd77f77bdb31aad39eb4952d
Zaza Sichinava <zazasichina@gmail.com>
2022-02-22 12:35:08 +0400

    2164. Sort Even and Odd Indices Independently

---
+++ 2164. Sort Even and Odd Indices Independently/leetcode_2162.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def sortEvenOdd(self, nums: List[int]) -> List[int]:
+        nums[::2] = sorted(nums[::2])
+        nums[1::2] = sorted(nums[1::2], reverse=True)
+        return nums
