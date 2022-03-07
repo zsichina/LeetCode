fb1d7c62de9c7d342ebe938bb217b60ce3bb5702
Zaza Sichinava <zazasichina@gmail.com>
2022-03-07 17:47:01 +0400

    2191. Sort the Jumbled Numbers

---
+++ 2191. Sort the Jumbled Numbers/leetcode_2191.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        def mapNum(num):
+            ans = ""
+            for n in str(num):
+                ans += str(mapping[int(n)])
+            return int(ans)
+
+        nums = sorted(nums, key=lambda x: mapNum(x))
+
+        return nums
