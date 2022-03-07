8acfbc903d42d51b89788a9c7415c4cee1689a81
Zaza Sichinava <zazasichina@gmail.com>
2022-03-07 17:26:10 +0400

    2190. Most Frequent Number Following Key In an Array

---
+++ 2190. Most Frequent Number Following Key In an Array/leetcode_2190.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def mostFrequent(self, nums: List[int], key: int) -> int:
+        d = defaultdict(int)
+        maxVal = 0
+        ans = -1
+        for i in range(len(nums) - 1):
+            if nums[i] == key:
+                d[nums[i + 1]] += 1
+                if d[nums[i + 1]] > maxVal:
+                    maxVal = d[nums[i + 1]]
+                    ans = nums[i + 1]
+        return ans
