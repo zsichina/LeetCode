e7577a4bbfc2c7e5a13e6e2401a26561a78dddc5
Zaza Sichinava <zazasichina@gmail.com>
2022-03-13 15:16:16 +0400

    1. Two Sum

---
+++ 1. Two Sum/leetcode_1.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] in d:
+                return [d[nums[i]], i]
+            d[target - nums[i]] = i
