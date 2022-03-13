001678a6cbdf6869575576ed23afb56b934516bb
Zaza Sichinava <zazasichina@gmail.com>
2022-03-13 11:39:57 +0400

    2195. Append K Integers With Minimal Sum

---
+++ 2195. Append K Integers With Minimal Sum/leetcode_2195.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def minimalKSum(self, nums: List[int], k: int) -> int:
+      sm = int(k*(k+1)/2)
+      #if k < min(nums): return sm
+      nums.sort()
+      for i in range(len(nums)):
+        if i > 0 and nums[i]==nums[i-1]:
+          continue
+        elif nums[i]<=k:
+          k+=1
+          sm=sm+k-nums[i]
+        else:
+          break
+      return sm
