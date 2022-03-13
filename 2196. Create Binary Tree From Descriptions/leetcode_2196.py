9a120dc2e1f7d0e4283056afeb47a89f2ae39486
Zaza Sichinava <zazasichina@gmail.com>
2022-03-13 12:07:23 +0400

    2196. Create Binary Tree From Descriptions

---
+++ 2196. Create Binary Tree From Descriptions/leetcode_2196.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        parents = set()
+        children = set()
+        desc = defaultdict()
+        for p, c, lr in descriptions:
+            parents.add(p)
+            children.add(c)
+            if p not in desc:
+                desc[p] = TreeNode(p)
+            if c not in desc:
+                desc[c] = TreeNode(c)
+            if lr == 1:
+                desc[p].left = desc[c]
+            else:
+                desc[p].right = desc[c]
+
+        return desc[list(parents.difference(children))[0]]
