73c9115c27796b8674f6e13adc2777f2fe467fea
Zaza Sichinava <zazasichina@gmail.com>
2022-03-14 12:15:29 +0400

    10. Regular Expression Matching

---
+++ 0010. Regular Expression Matching/leetcode_10.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+
+class Solution:
+    @lru_cache
+    def isMatch(self, s: str, p: str) -> bool:
+        if not p:
+            return not s
+
+        first_match = bool(s) and p[0] in {s[0], '.'}
+
+        if len(p) >= 2 and p[1] == '*':
+            return (self.isMatch(s, p[2:]) or
+                    first_match and self.isMatch(s[1:], p))
+        else:
+            return first_match and self.isMatch(s[1:], p[1:])
