fb3a49fb495ccad9a0d23e475898dc6b2861fcdd
Zaza Sichinava <zazasichina@gmail.com>
2022-03-14 10:34:01 +0400

    6. Zigzag Conversion

---
+++ 0006. Zigzag Conversion/leetcode_6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if len(s) == 1 or numRows == 1: return s
+        arr = ['' for x in range(min(numRows, len(s)))]
+        goingUp = True
+        currRow = 0
+        for val in s:
+            arr[currRow] += val
+            if (currRow == 0 or currRow == numRows - 1): goingUp = not goingUp
+            currRow += -1 if goingUp else 1
+        return ''.join(arr)
