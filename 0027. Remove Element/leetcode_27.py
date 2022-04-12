8c68792ab3f878fb847f4c4828c428082715169c
Zaza Sichinava <zazasichina@gmail.com>
2022-04-12 15:28:06 +0400

    27. Remove Element

---
+++ 0027. Remove Element/leetcode_27.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
