b884841b5cb98e0fbfb136e42c3391f0f97607fa
Zaza Sichinava <zazasichina@gmail.com>
2022-04-12 15:02:42 +0400

    25. Reverse Nodes in k-Group

---
+++ 0025. Reverse Nodes in k-Group/leetcode_25.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head: return None
+        arr = []
+        curr = head
+        for i in range(k):
+            if not curr:
+                arr = arr[::-1]
+                break
+            arr.append(curr)
+            curr = curr.next
+            arr[-1].next = None
+
+        head = arr.pop()
+        curr1 = head
+        for i in range(len(arr)):
+            curr1.next = arr.pop()
+            curr1 = curr1.next
+
+        if curr:
+            curr1.next = self.reverseKGroup(curr, k)
+
+        return head
