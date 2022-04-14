5f5a4e343ec2432262693e2f445aa9ea01cc5847
Zaza Sichinava <zazasichina@gmail.com>
2022-04-14 12:07:55 +0400

    41. First Missing Positive

---
+++ 0041. First Missing Positive/leetcode_41.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            while (nums[i] != i+1 and 1 <= nums[i] <= len(nums) and nums[i] != nums[nums[i]-1]):
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                return i+1
+        return len(nums)+1
