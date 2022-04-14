f01f5564244421e3ae0748eb61740daadcf7bd6d
Zaza Sichinava <zazasichina@gmail.com>
2022-04-14 11:49:51 +0400

    38. Count and Say

---
+++ 0038. Count and Say/leetcode_38.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1: return "1"
+        toSay = self.countAndSay(n - 1)
+        currChar = toSay[0]
+        cnt = 0
+        res = ""
+        for c in toSay:
+            if c == currChar:
+                cnt += 1
+            else:
+                res += str(cnt) + currChar
+                cnt = 1
+                currChar = c
+        res += str(cnt) + currChar
+
+        return res
