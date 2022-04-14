f6a9bd5107e447c3922b663e91e6fb71646dc254
Zaza Sichinava <zazasichina@gmail.com>
2022-04-14 11:55:40 +0400

    39. Combination Sum

---
+++ 0039. Combination Sum/leetcode_39.py
@@ -0,0 +1,11 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+        for idx, num in enumerate(candidates):
+            if target % num == 0:
+                res += [[num] * (target // num)]
+            if idx < len(candidates) - 1:
+                for i in range(1, target // num):
+                    res += [[num] * i + x for x in self.combinationSum(candidates[idx + 1:], target - num * i)]
+        return res
