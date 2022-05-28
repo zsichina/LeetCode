b98fe9123b41f4a46b6050a06aec88bb7694729d
Zaza Sichinava <zazasichina@gmail.com>
2022-05-28 11:57:52 +0400

    53. Maximum Subarray

---
+++ 0053. Maximum Subarray/leetcode_53.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sm, maxSum = 0, -10**4
+        for val in nums:
+            sm += val
+            maxSum = sm if sm > maxSum else maxSum
+            sm = 0 if sm < 0 else sm
+        return maxSum
