b95c1c26a54c245824736f343e2fd23ee302f549
Zaza Sichinava <zazasichina@gmail.com>
2022-07-06 14:53:46 +0400

    57. Insert Interval

---
+++ 0057. Insert Interval/leetcode_57.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        if n == 0:
+            return [newInterval]
+
+        start, end = 0, n
+        for i in range(n):
+            if newInterval[0] > intervals[i][1]:
+                start += 1
+            if newInterval[1] < intervals[n-1-i][0]:
+                end -= 1
+            
+        if start == end:
+            intervals.insert(start, newInterval)
+            return intervals
+        
+        intervals[start:end] = [[min(intervals[start][0], newInterval[0]), max(intervals[end-1][1], newInterval[1])]]
+
+        return intervals
+
+
+
