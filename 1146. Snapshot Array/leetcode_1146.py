393d1010b1dbb5e0ea461901fcb80859f5059203
Zaza Sichinava <zazasichina@gmail.com>
2022-07-18 14:29:54 +0400

    1146. Snapshot Array

--- 1146. Snapshot Array/leetcode_1146.py
+++ 1146. Snapshot Array/leetcode_1146.py
@@ -21,3 +21,31 @@ class SnapshotArray:
             return self.snaps[index][snap_id]
         return 0
 
+
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.snaps = defaultdict(list)
+        self.snap_cnt = 0
+
+    def set(self, index: int, val: int) -> None:
+        if self.snaps[index] and self.snaps[index][-1][0] == self.snap_cnt:
+            self.snaps[index][-1][1] = val
+        else:
+            self.snaps[index].append([self.snap_cnt, val])
+
+    def snap(self) -> int:
+        self.snap_cnt += 1
+        return self.snap_cnt - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        start, end, last_snap_id = 0, len(self.snaps[index]) - 1, -1
+        while start <= end:
+            mid = (start + end)//2
+            if self.snaps[index][mid][0] <= snap_id:
+                start = mid + 1
+                last_snap_id = mid
+            else:
+                end = mid -1
+                    
+        return 0 if last_snap_id == -1 else self.snaps[index][last_snap_id][1]
