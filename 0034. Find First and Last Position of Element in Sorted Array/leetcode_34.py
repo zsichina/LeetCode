7fe613a98f8a9ec9f852187c043d7967f01b6586
Zaza Sichinava <zazasichina@gmail.com>
2022-07-25 11:51:16 +0400

    34. Find First and Last Position of Element in Sorted Array

--- 0034. Find First and Last Position of Element in Sorted Array/leetcode_34.py
+++ 0034. Find First and Last Position of Element in Sorted Array/leetcode_34.py
@@ -2,28 +2,31 @@ from typing import List
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def findTarget(left, right, leftRightBound=None):
-            res = [-1, -1]
-            if left > right: return res
-            mid = left + (right - left + 1) // 2
-            if nums[mid] == target:
-                if leftRightBound is None or leftRightBound == "L":
-                    if mid == 0 or nums[mid - 1] < target:
-                        res[0] = mid
+        def find_bounds(start, end, find_left = True):
+            
+            while start <= end:
+                mid = (start + end) // 2
+                if nums[mid] == target:
+                    if find_left:
+                        if mid == 0 or nums[mid-1] != target:
+                            return mid
+                        end = mid - 1
                     else:
-                        tempRes = findTarget(left, mid - 1, "L")
-                        res[0] = tempRes[0]
-                if leftRightBound is None or leftRightBound == "R":
-                    if mid == len(nums) - 1 or nums[mid + 1] > target:
-                        res[1] = mid
-                    else:
-                        tempRes = findTarget(mid + 1, right, "R")
-                        res[1] = tempRes[1]
-                return res
-            elif nums[mid] > target and mid > 0:
-                res = findTarget(left, mid - 1, leftRightBound)
-            elif nums[mid] < target and mid < len(nums) + 1:
-                res = findTarget(mid + 1, right, leftRightBound)
-            return res
-
-        return findTarget(0, len(nums) - 1)
+                        if mid == len(nums) - 1 or nums[mid+1] != target:
+                            return mid
+                        start = mid + 1
+                elif nums[mid] < target:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            
+            return -1
+        
+        left = find_bounds(0, len(nums) - 1)
+        
+        if left == -1:
+            return [-1,-1]
+        
+        right = find_bounds(left, len(nums) - 1, False)
+        
+        return [left, right]
