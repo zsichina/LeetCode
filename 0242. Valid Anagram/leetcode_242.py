00bc02e2cfabb9f79a9c8a30bb8e8246cad841dd
Zaza Sichinava <zazasichina@gmail.com>
2022-07-28 09:30:52 +0400

    242. Valid Anagram

---
+++ 0242. Valid Anagram/leetcode_242.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_counter = Counter(s)
+        t_counter = Counter(t)
+        
+        for key, val in s_counter.items():
+            if not key in t_counter or t_counter[key] != val:
+                return False
+        
+        return True
