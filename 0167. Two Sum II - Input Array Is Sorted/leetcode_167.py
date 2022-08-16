8797a35b147d7a55c89423c87cccf6c0c569ae41
Zaza Sichinava <zazasichina@gmail.com>
2022-08-16 13:38:28 +0400

    167. Two Sum II - Input Array Is Sorted

--- 0167. Two Sum II - Input Array Is Sorted/leetcode_167.py
+++ 0167. Two Sum II - Input Array Is Sorted/leetcode_167.py
@@ -8,8 +8,7 @@ class Solution:
         while start < end:
             if numbers[start] + numbers[end] == target:
                 return [start + 1, end + 1]
-
-            if numbers[start] + numbers[end] < target:
+            elif numbers[start] + numbers[end] < target:
                 start += 1
             else:
                 end -= 1
