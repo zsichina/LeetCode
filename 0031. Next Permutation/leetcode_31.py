c81f271a9f2daf660e71bbae1b9fd61741ea295a
Zaza Sichinava <zazasichina@gmail.com>
2022-08-17 18:28:35 +0400

    31. Next Permutation. Corrected naming

--- 0031. Next Permutation/leetcode_31.py
+++ 0031. Next Permutation/leetcode_31.py
@@ -2,17 +2,19 @@ from typing import List
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        maxIdx = len(nums)-1
-        for i in range(len(nums) -1, 0, -1):
-            if nums[i] > nums[maxIdx]:
-                maxIdx = i
-            low = maxIdx
-            if nums[maxIdx] > nums[i-1]:
-                for j in range(len(nums)-i):
-                    if nums[i+j] > nums[i-1] and nums[i+j] < nums[low]:
-                        low = i+j
-                nums[low], nums[i-1] = nums[i-1], nums[low]
-                nums[i:] = sorted(nums[i:])
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        for i in range(n-2, -1, -1):
+            if nums[i+1] > nums[i]:
+                max_idx = i+1
+                for j in range(i+1, n):
+                    if nums[j] > nums[i] and nums[j] < nums[max_idx]:
+                        max_idx = j
+
+                nums[max_idx], nums[i] = nums[i], nums[max_idx]
+                nums[i+1:] = sorted(nums[i+1:])
                 return
 
         nums.sort()
