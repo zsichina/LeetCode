9e139f68f0cf3849f5740d1182e05165c3bcce3a
Zaza Sichinava <zazasichina@gmail.com>
2022-08-17 15:19:57 +0400

    48. Rotate Image. Added solutions

--- 0048. Rotate Image/leetcode_48.py
+++ 0048. Rotate Image/leetcode_48.py
@@ -9,3 +9,52 @@ class Solution:
         n = len(matrix)
         for i in range(n):
             rotate90(n-1, i)
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        offset = 0
+        while n - 2*offset > 1:
+            for i in range(n-1-2*offset):
+                matrix[offset][offset+i], matrix[n-1-offset-i][offset] = matrix[n-1-offset-i][offset], matrix[offset][offset+i]
+                matrix[n-1-offset-i][offset], matrix[n-1-offset][n-1-offset-i] = matrix[n-1-offset][n-1-offset-i], matrix[n-1-offset-i][offset]
+                matrix[n-1-offset][n-1-offset-i], matrix[offset+i][n-1-offset] = matrix[offset+i][n-1-offset], matrix[n-1-offset][n-1-offset-i]
+
+            offset += 1
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        offset = 0
+        while n - 2*offset > 1:
+            for i in range(n-1-2*offset):
+                temp = matrix[offset][offset+i]
+                matrix[offset][offset+i] = matrix[n-1-offset-i][offset]
+                matrix[n-1-offset-i][offset] = matrix[n-1-offset][n-1-offset-i]
+                matrix[n-1-offset][n-1-offset-i] = matrix[offset+i][n-1-offset]
+                matrix[offset+i][n-1-offset] = temp
+
+            offset += 1
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i+1):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
