758032f894a2ff1c60fbf81700c5c57eec878381
Zaza Sichinava <zazasichina@gmail.com>
2022-08-17 11:00:54 +0400

    804. Unique Morse Code Words

---
+++ 0804. Unique Morse Code Words/leetcode_804.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse_code_map = [
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+            ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+            "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
+        ]
+        transformed_words = set()
+        offset = ord("a")
+        for word in words:
+            transformed_word = ""
+            for letter in word:
+                idx = ord(letter) - offset
+                transformed_word += morse_code_map[idx]
+            transformed_words.add(transformed_word)
+
+        return len(transformed_words)
+
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse_code_map = [
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+            ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+            "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
+        ]
+
+        transformed_words = {
+            ("".join(morse_code_map[ord(l) - ord(a)]) for l in w) for w in words
+        }
+
+        return len(transformed_words)
