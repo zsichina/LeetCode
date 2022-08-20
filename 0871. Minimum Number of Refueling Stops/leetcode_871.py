27ca6d72af18ba3a5b317d7af23198154d1ab616
Zaza Sichinava <zazasichina@gmail.com>
2022-08-20 12:05:31 +0400

    871. Minimum Number of Refueling Stops. Appended target to stations

--- 0871. Minimum Number of Refueling Stops/leetcode_871.py
+++ 0871. Minimum Number of Refueling Stops/leetcode_871.py
@@ -1,12 +1,14 @@
+import math
+
 from typing import List
-from heapq import heapify, heappush, heappop
+from heapq import heappush, heappop
 
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
         h = []
-        heapify(h)
         fuel = startFuel
         cnt = 0
+        stations.append([target, math.inf])
         for i in range(len(stations)):
             
             fuel -= stations[i][0] if i == 0 else stations[i][0] - stations[i-1][0]
@@ -19,14 +21,5 @@ class Solution:
                 return -1
             
             heappush(h, -stations[i][1])
-
-        fuel -= target - stations[-1][0] if stations else target
-
-        while h and fuel < 0:
-            fuel -= heappop(h)
-            cnt += 1
-
-        if fuel < 0:
-            return -1
 
         return cnt
