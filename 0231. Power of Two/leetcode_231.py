05a05c49c76e3856eb81d94bc4668cf00217c5a9
Zaza Sichinava <zazasichina@gmail.com>
2022-08-22 12:48:54 +0400

    231. Power of Two

---
+++ 0231. Power of Two/leetcode_231.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n == 0:
+            return False
+        return n & (-n) == n
+
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n == 0:
+            return False
+        return n & (n - 1) == 0
