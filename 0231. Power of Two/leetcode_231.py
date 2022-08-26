d74bfbb6994c27a06982225d73b04e777d238d89
Zaza Sichinava <zazasichina@gmail.com>
2022-08-26 11:51:39 +0400

    231. Power of Two. Added solution

--- 0231. Power of Two/leetcode_231.py
+++ 0231. Power of Two/leetcode_231.py
@@ -10,3 +10,10 @@ class Solution(object):
         if n == 0:
             return False
         return n & (n - 1) == 0
+
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n <= 0:
+            return False
+        return bin(n).count('1') == 1
