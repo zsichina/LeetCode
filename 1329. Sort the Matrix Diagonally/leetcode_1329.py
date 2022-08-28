a21db2aeb0d9619e46dc5beb784481f538a55e1d
Zaza Sichinava <zazasichina@gmail.com>
2022-08-28 13:44:59 +0400

    1329. Sort the Matrix Diagonally. Added solution with countSort

--- 1329. Sort the Matrix Diagonally/leetcode_1329.py
+++ 1329. Sort the Matrix Diagonally/leetcode_1329.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from heapq import heapify, heappop
 from typing import List
 
@@ -42,6 +43,44 @@ class Solution:
 
             for i in range(diag_len):
                 mat[row + i][col + i] = heappop(diag)
+
+        for row in range(m):
+            sortDiagonal(row, 0)
+
+        for col in range(1, n):
+            sortDiagonal(0, col)
+
+        return mat
+
+
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+
+        def sortDiagonal(row, col):
+            diag = []
+            diag_len = min(m-row, n-col)
+
+            for i in range(diag_len):
+                diag.append(mat[row + i][col + i])
+
+            diag = countingSort(diag)
+
+            for i in range(diag_len):
+                mat[row + i][col + i] = diag[i]
+
+        def countingSort(nums):
+            minimum = 1
+            maximum = 100
+
+            counts = Counter(nums)
+
+            sorted_nums = []
+            for i in range(minimum, maximum + 1):
+                sorted_nums.extend([i] * counts[i])
+            return sorted_nums
+
 
         for row in range(m):
             sortDiagonal(row, 0)
