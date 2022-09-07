a92764237bda05df67c9496836a05d1ea8194924
Zaza Sichinava <zazasichina@gmail.com>
2022-09-07 11:07:34 +0400

    158. Read N Characters Given read4 II - Call Multiple Times

---
+++ 0158. Read N Characters Given read4 II - Call Multiple Times/leetcode_158.py
@@ -0,0 +1,27 @@
+# The read4 API is already defined for you.
+# def read4(buf4: List[str]) -> int:
+
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        self.chars_read = 0
+        self.chars_used = 0
+        self.buf4 = ["" for _ in range(4)]
+
+    def read(self, buf: List[str], n: int) -> int:
+        cnt = 0
+        while cnt < n:
+            if self.chars_used == self.chars_read:
+                self.chars_read = read4(self.buf4)
+                self.chars_used = 0
+
+            if self.chars_read == 0:
+                return cnt
+
+            buf[cnt] = self.buf4[self.chars_used]
+            self.chars_used += 1
+            cnt += 1
+
+        return cnt
