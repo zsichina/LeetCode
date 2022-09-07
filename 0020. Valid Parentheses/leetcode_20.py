1ea7ec82de1bbfde88510f199b1f4794cd1b5a3e
Zaza Sichinava <zazasichina@gmail.com>
2022-09-07 18:06:07 +0400

    20. Valid Parentheses. Replaced solution with new one

--- 0020. Valid Parentheses/leetcode_20.py
+++ 0020. Valid Parentheses/leetcode_20.py
@@ -1,12 +1,14 @@
 class Solution:
-  def isValid(self, s: str) -> bool:
-    if len(s)%2 != 0: return
-    stack = []
-    open = "([{"
-    close = ")]}"
-    for c in s:
-      if c in open:
-        stack.append(c)
-      elif len(stack) == 0 or c != close[open.find(stack.pop())]:
-        return False
-    return len(stack) == 0
+    def isValid(self, s: str) -> bool:
+        if len(s)%2 != 0:
+            return False
+
+        d = {"(" : ")", "[" : "]", "{" : "}"}
+        stack = []
+        for bracket in s:
+            if bracket in d:
+                stack.append(bracket)
+            elif not stack or d[stack.pop()] != bracket:
+                return False
+
+        return not stack
