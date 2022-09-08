64f636f51510839615f36d482154fdf528885133
Zaza Sichinava <zazasichina@gmail.com>
2022-09-08 13:49:50 +0400

    362. Design Hit Counter. Added new solution

--- 0362. Design Hit Counter/leetcode_362.py
+++ 0362. Design Hit Counter/leetcode_362.py
@@ -1,3 +1,4 @@
+from collections import deque
 import bisect
 
 
@@ -21,3 +22,26 @@ class HitCounter:
         start = bisect.bisect(self.timestamps, max(0, timestamp-300)) - 1
 
         return self.total_hits[end] - self.total_hits[start]
+
+
+class HitCounter:
+
+    def __init__(self):
+        self.hits = deque()
+        self.total = 0
+
+
+    def hit(self, timestamp: int) -> None:
+        self.total += 1
+        if not self.hits or self.hits[-1][0] != timestamp:
+            self.hits.append([timestamp, 1])
+        else:
+            self.hits[-1][1] += 1
+
+
+    def getHits(self, timestamp: int) -> int:
+        while self.hits and self.hits[0][0] <= timestamp - 300:
+            hits = self.hits.popleft()
+            self.total -= hits[1]
+
+        return self.total
