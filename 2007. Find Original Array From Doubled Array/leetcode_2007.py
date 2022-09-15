6f4db78d8940d66ee34fd9ceebd9ffac212f1223
Zaza Sichinava <zazasichina@gmail.com>
2022-09-15 16:30:58 +0400

    2007. Find Original Array From Doubled Array. Added solution with counting sort

--- 2007. Find Original Array From Doubled Array/leetcode_2007.py
+++ 2007. Find Original Array From Doubled Array/leetcode_2007.py
@@ -29,3 +29,28 @@ class Solution:
                 res.append(changed[i])
 
         return res
+
+
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        n = len(changed)
+        if not n or n%2 == 1:
+            return []
+
+        min_val, max_val = min(changed), max(changed)
+        dp = [0 for _ in range(max_val-min_val+1)]
+
+        for val in changed:
+            dp[val-min_val] += 1
+
+        res = []
+        for i in range(len(dp)):
+            while dp[i] > 0:
+                dp[i] -= 1
+                if (i+min_val)*2 > max_val or dp[(i+min_val)*2-min_val] == 0:
+                    return []
+                else:
+                    res.append(i+min_val)
+                    dp[(i+min_val)*2-min_val] -= 1
+
+        return res
