8d3becd4e5316159a9dcb59fcfda6acc888248fd
Zaza Sichinava <zazasichina@gmail.com>
2022-09-18 11:19:29 +0400

    42. Trapping Rain Water

--- 0042. Trapping Rain Water/leetcode_42.py
+++ 0042. Trapping Rain Water/leetcode_42.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def trap(self, height: List[int]) -> int:
         res = 0
@@ -15,4 +16,22 @@ class Solution:
                 res += distance * bounded_height
             st.append(curr)
             curr += 1
+
+        return res
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        res, n = 0, len(height)
+        left = [0 for _ in range(n)]
+        right = [0 for _ in range(n)]
+        left[0], right[n-1] = height[0], height[n-1]
+
+        for i in range(1, n):
+            left[i] = max(left[i-1], height[i])
+            right[n-1-i] = max(right[n-i], height[n-1-i])
+
+        for i in range(n):
+            res += min(left[i], right[i]) - height[i]
+
         return res
