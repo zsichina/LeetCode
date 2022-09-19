29d97e19f06d841bd3fceee20861271ede511f58
Zaza Sichinava <zazasichina@gmail.com>
2022-09-19 11:31:38 +0400

    609. Find Duplicate File in System

---
+++ 0609. Find Duplicate File in System/leetcode_609.py
@@ -0,0 +1,19 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        dp = defaultdict(list)
+        for path in paths:
+            path_lst = path.split()
+            for filename in path_lst[1:]:
+                name, content = filename.split("(")
+                dp[content].append(path_lst[0] + "/" + name)
+
+        res = []
+        for _, val in dp.items():
+            if len(val) > 1:
+                res.append(val)
+
+        return res
