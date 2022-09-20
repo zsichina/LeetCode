3835321e4ccb1bb56e1c5dbc8181a299c8c56b69
Zaza Sichinava <zazasichina@gmail.com>
2022-09-20 16:29:13 +0400

    844. Backspace String Compare. Added solution with constans space

--- 0844. Backspace String Compare/leetcode_844.py
+++ 0844. Backspace String Compare/leetcode_844.py
@@ -18,3 +18,31 @@ class Solution:
         return "".join(s_typed) == "".join(t_typed)
 
 
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def skip(s, pointer):
+            skip = 0
+            while pointer >= 0 and (s[pointer] == "#" or skip):
+                if s[pointer] == "#":
+                    skip += 1
+                else:
+                    skip -= 1
+                pointer -= 1
+
+            return pointer
+
+        p1, p2 = len(s)-1, len(t)-1
+        while p1 >= 0 or p2 >= 0:
+            p1 = skip(s, p1)
+            p2 = skip(t, p2)
+
+            if p1 > -1 and p2 > -1:
+                if s[p1] != t[p2]:
+                    return False
+                else:
+                    p1 -= 1
+                    p2 -= 1
+            elif p1 > -1 and p2 == -1 or p1 == -1 and p2 > -1:
+                return False
+
+        return True
