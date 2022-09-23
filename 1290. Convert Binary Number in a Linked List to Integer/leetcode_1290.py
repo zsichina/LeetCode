33354ebeb00ce218d1b0dce4ff3b3624d44ecff2
Zaza Sichinava <zazasichina@gmail.com>
2022-09-23 10:48:11 +0400

    1290. Convert Binary Number in a Linked List to Integer

---
+++ 1290. Convert Binary Number in a Linked List to Integer/leetcode_1290.py
@@ -0,0 +1,38 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        total = 0
+        curr = head
+        while curr:
+            total = total*2+curr.val
+            curr = curr.next
+
+        return total
+
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        total = 0
+        curr = head
+        while curr:
+            total *= 2
+            total |= curr.val
+            curr = curr.next
+
+        return total
+
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        total = 0
+        curr = head
+        while curr:
+            total = (total << 1) | curr.val
+            curr = curr.next
+
+        return total
