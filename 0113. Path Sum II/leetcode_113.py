9c9fd5fdd718107583c3bd772c3d4e745d7d7863
Zaza Sichinava <zazasichina@gmail.com>
2022-09-24 12:12:30 +0400

    113. Path Sum II

---
+++ 0113. Path Sum II/leetcode_113.py
@@ -0,0 +1,30 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if not root:
+            return []
+        def dfs(root, sm, lst):
+            sm += root.val
+            lst.append(root.val)
+            if root.left:
+                dfs(root.left, sm, lst)
+            if root.right:
+                dfs(root.right, sm, lst)
+
+            if not root.left and not root.right and sm == targetSum:
+                res.append(lst[:])
+            lst.pop()
+
+        res = []
+        dfs(root, 0, [])
+
+        return res
