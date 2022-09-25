7191a2ca13d75be8f82e9d4972f8b05e2326b928
Zaza Sichinava <zazasichina@gmail.com>
2022-09-25 12:18:21 +0400

    1114. Print in Order

---
+++ 1114. Print in Order/leetcoce_1114.py
@@ -0,0 +1,26 @@
+from typing import Callable
+from threading import Lock
+
+
+class Foo:
+    def __init__(self):
+        self.firstJobDone = Lock()
+        self.secondJobDone = Lock()
+        self.firstJobDone.acquire()
+        self.secondJobDone.acquire()
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        printFirst()
+        self.firstJobDone.release()
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+         with self.firstJobDone:
+            printSecond()
+            self.secondJobDone.release()
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        with self.secondJobDone:
+            printThird()
