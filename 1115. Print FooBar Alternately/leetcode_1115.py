fc9f8eb73fb54ee8ffe41dc251124466a2b7c45a
Zaza Sichinava <zazasichina@gmail.com>
2022-09-25 14:00:56 +0400

    1115. Print FooBar Alternately

---
+++ 1115. Print FooBar Alternately/leetcode_1115.py
@@ -0,0 +1,25 @@
+from typing import Callable
+from threading import Lock
+
+
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.foo_done = Lock()
+        self.bar_done = Lock()
+        self.foo_done.acquire()
+
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+
+        for i in range(self.n):
+            self.bar_done.acquire()
+            printFoo()
+            self.foo_done.release()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        
+        for i in range(self.n):
+            self.foo_done.acquire()
+            printBar()
+            self.bar_done.release()
