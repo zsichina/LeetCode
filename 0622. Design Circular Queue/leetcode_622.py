27b5fb7c69c2df0c7e4fae28c089c36f047ad335
Zaza Sichinava <zazasichina@gmail.com>
2022-09-25 11:46:22 +0400

    622. Design Circular Queue. Added thread safe solution

--- 0622. Design Circular Queue/leetcode_622.py
+++ 0622. Design Circular Queue/leetcode_622.py
@@ -35,3 +35,49 @@ class MyCircularQueue:
         if self.circularQueue[self.end] == -1 or self.start != self.end:
             return False
         return True
+
+
+
+from threading import Lock
+
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.size = k
+        self.circularQueue = [-1 for _ in range(k)]
+        self.start = 0
+        self.end = 0
+        self.queueLock = Lock()
+
+    def enQueue(self, value: int) -> bool:
+        with self.queueLock:
+            if self.isFull():
+                return False
+            self.circularQueue[self.end] = value
+            self.end = (self.end+1)%self.size
+        return True
+
+    def deQueue(self) -> bool:
+        with self.queueLock:
+            if self.isEmpty():
+                return False
+            self.circularQueue[self.start] = -1
+            self.start = (self.start+1)%self.size
+        return True
+
+    def Front(self) -> int:
+        return self.circularQueue[self.start]
+
+    def Rear(self) -> int:
+        return self.circularQueue[self.end-1]
+
+    def isEmpty(self) -> bool:
+        if self.circularQueue[self.start] == -1:
+            return True
+        return False
+
+    def isFull(self) -> bool:
+        if self.circularQueue[self.end] == -1 or self.start != self.end:
+            return False
+        return True
