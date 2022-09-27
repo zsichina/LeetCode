c90c966b3377c408093c27846a804726122baaf8
Zaza Sichinava <zazasichina@gmail.com>
2022-09-27 12:05:48 +0400

    838. Push Dominoes

---
+++ 0838. Push Dominoes/leetcode_838.py
@@ -0,0 +1,25 @@
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        start, cnt = 0, 0
+        dominoes = ["L"] + list(dominoes) + ["R"]
+        for i in range(len(dominoes)):
+            if dominoes[i] == ".":
+                cnt += 1
+            elif dominoes[i] == "L":
+                if dominoes[start] == "L":
+                    for j in range(cnt):
+                        dominoes[start+1+j] = "L"
+                elif dominoes[start] == "R":
+                    for j in range(cnt//2):
+                        dominoes[start+1+j] = "R"
+                        dominoes[i-1-j] = "L"
+                start = i
+                cnt = 0
+            else:
+                if dominoes[start] == "R":
+                    for j in range(cnt):
+                        dominoes[start+1+j] = "R"
+                start = i
+                cnt = 0
+
+        return "".join(dominoes[1:-1])
