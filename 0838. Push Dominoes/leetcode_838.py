42b5edd2ff22e26ed4aa1881eb22cf643ec4efdb
Zaza Sichinava <zazasichina@gmail.com>
2022-09-29 13:48:43 +0400

    838. Push Dominoes. Added refactored code

--- 0838. Push Dominoes/leetcode_838.py
+++ 0838. Push Dominoes/leetcode_838.py
@@ -23,3 +23,24 @@ class Solution:
                 cnt = 0
 
         return "".join(dominoes[1:-1])
+
+
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        start, cnt = 0, 0
+        dominoes = ["L"] + list(dominoes) + ["R"]
+        for i in range(len(dominoes)):
+            if dominoes[i] == ".":
+                cnt += 1
+            else:
+                if dominoes[i] == dominoes[start]:
+                    for j in range(cnt):
+                        dominoes[start+1+j] = dominoes[start]
+                elif dominoes[start] == "R":
+                    for j in range(cnt//2):
+                        dominoes[start+1+j] = "R"
+                        dominoes[i-1-j] = "L" 
+                start = i
+                cnt = 0
+
+        return "".join(dominoes[1:-1])
