efbf18560c205da5300cc7585ca59ebb62aadd6f
zsichina <zazasichina@gmail.com>
2022-10-01 12:14:29 +0400

    91. Decode Ways. Added constant space solution

--- 0091. Decode Ways/leetcode_91.py
+++ 0091. Decode Ways/leetcode_91.py
@@ -29,3 +29,23 @@ class Solution:
             return res
 
         return helper(0, s, len(s))
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == "0":
+            return 0
+
+        two_back = 1
+        one_back = 1
+        for i in range(1, len(s)):
+            current = 0
+            if s[i] != "0":
+                current = one_back
+            two_digit = int(s[i - 1: i + 1])
+            if two_digit >= 10 and two_digit <= 26:
+                current += two_back
+            two_back = one_back
+            one_back = current
+
+        return one_back
