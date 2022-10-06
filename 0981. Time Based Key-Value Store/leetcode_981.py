08b4c80b5516bd11192f5e0a9690cf80a1a32a22
Zaza Sichinava <zazasichina@gmail.com>
2022-10-06 17:08:46 +0400

    981. Time Based Key-Value Store

---
+++ 0981. Time Based Key-Value Store/leetcode_981.py
@@ -0,0 +1,26 @@
+from bisect import bisect_right
+
+
+class TimeMap:
+
+    def __init__(self):
+        self.dp = {}
+        self.dp_val = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.dp:
+          self.dp[key] = [timestamp]
+          self.dp_val[key] = [value]
+        else:
+          self.dp[key].append(timestamp)
+          self.dp_val[key].append(value)
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.dp:
+          return ""
+        idx = bisect_right(self.dp[key], timestamp)
+        if self.dp[key][idx-1] == timestamp:
+          return self.dp_val[key][idx-1]
+        if idx == 0:
+          return ""
+        return self.dp_val[key][idx-1]
