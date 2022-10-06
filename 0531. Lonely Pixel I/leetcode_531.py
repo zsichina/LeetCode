74bcbe03cc61a661dc53ebb8aaafde41056a2b1f
Zaza Sichinava <zazasichina@gmail.com>
2022-10-06 18:44:05 +0400

    531. Lonely Pixel I

---
+++ 0531. Lonely Pixel I/leetcode_531.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def findLonelyPixel(self, picture: List[List[str]]) -> int:
+        m, n = len(picture), len(picture[0])
+        row = [0 for _ in range(m)]
+        col = [0 for _ in range(n)]
+
+        for i in range(m):
+          for j in range(n):
+            if picture[i][j] == "B":
+              row[i] += 1
+              col[j] += 1
+
+        res = 0
+        for i in range(m):
+          for j in range(n):
+            if picture[i][j] == "B" and row[i] == 1 and col[j] == 1:
+              res += 1
+              
+        return res
