5b5246c03a327c5d2b5bb7684f0bdf2032e9028a
Zaza Sichinava <zazasichina@gmail.com>
2022-10-12 21:45:58 +0400

    976. Largest Perimeter Triangle

---
+++ 0976. Largest Perimeter Triangle/leetcode_976.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums)-3, -1, -1):
+          if nums[i] + nums[i+1] > nums[i+2]:
+            return nums[i] + nums[i+1] + nums[i+2]
+        
+        return 0
