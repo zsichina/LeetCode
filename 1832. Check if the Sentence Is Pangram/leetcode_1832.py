8a1428861fe2201f6fe5939661556991608d2485
Zaza Sichinava <zazasichina@gmail.com>
2022-10-17 19:08:02 +0400

    1832. Check if the Sentence Is Pangram. Added solution with bit manipulation

--- 1832. Check if the Sentence Is Pangram/leetcode_1832.py
+++ 1832. Check if the Sentence Is Pangram/leetcode_1832.py
@@ -10,3 +10,17 @@ class Solution:
             return False
 
         return True
+
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        seen = 0
+
+        for curr_char in sentence:
+            mapped_index = ord(curr_char) - ord('a')
+
+            curr_bit = 1 << mapped_index 
+
+            seen |= curr_bit
+
+        return seen == (1 << 26) - 1
