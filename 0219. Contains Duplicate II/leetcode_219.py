a65083d6a368a06c45429e2dc7949b9c8b052db5
Zaza Sichinava <zazasichina@gmail.com>
2022-10-21 19:55:49 +0400

    219. Contains Duplicate II

---
+++ 0219. Contains Duplicate II/leetcode_219.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        dp = {}
+        for i in range(len(nums)):
+          if nums[i] in dp and abs(dp[nums[i]]-i) <= k:
+            return True
+          dp[nums[i]] = i
+        return False
