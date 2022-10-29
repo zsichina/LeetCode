3c53140233571b90fca6aee180f387674c3473f4
Zaza Sichinava <zazasichina@gmail.com>
2022-10-29 12:48:43 +0400

    2136. Earliest Possible Day of Full Bloom

---
+++ 2136. Earliest Possible Day of Full Bloom/leetcode_2136.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
+        lst = sorted(zip(growTime, plantTime), reverse=True)
+        spent = rem = 0
+        for g, p in lst:
+          spent += p
+          rem = max(0, rem-p)
+          rem = max(rem, g)
+
+        return spent + rem
