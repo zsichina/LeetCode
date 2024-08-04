2f4a4a51c9ba736c20a878e4caa52dfdc9265e1e
Zaza Sichina <zazasichina@gmail.com>
2024-08-04 07:40:41 +0200

    1508. Range Sum of Sorted Subarray Sums

---
+++ 1508. Range Sum of Sorted Subarray Sums/leetcode_1508.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def rangeSum(self, nums, n, left, right):
+        """
+        :type nums: List[int]
+        :type n: int
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        sums = []
+        for i in range(n):
+            for j in range(i+1, n+1):
+                if j == i+1:
+                    sm = nums[i]
+                else:
+                    sm = sums[-1] + nums[j-1]
+                    sums.append(sm)
+        sums.sort()
+        return sum(sums[left-1:right])%(10**9+7)
+                                                                                                                                                                                                                                  
