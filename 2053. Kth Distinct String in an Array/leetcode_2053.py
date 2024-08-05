e7ec840fbc38d0cad6890a3bfd7d4a7e7a060fca
Zaza Sichina <zazasichina@gmail.com>
2024-08-05 21:02:32 +0200

    2053. Kth Distinct String in an Array

---
+++ 2053. Kth Distinct String in an Array/leetcode_2053.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+
+class Solution(object):
+    def kthDistinct(self, arr, k):
+        """
+        :type arr: List[str]
+        :type k: int
+        :rtype: str
+        """
+        counter = Counter(arr)
+        cnt = 0
+        for elem in arr:
+            if counter[elem] == 1:
+                cnt +=1
+            if cnt == k:
+                return elem
+        return ""
