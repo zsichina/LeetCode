c76e39b3e6e959fb5fe7c8abcacdc596e012f9a7
Zaza Sichina <zazasichina@gmail.com>
2024-08-06 08:37:00 +0200

    3016. Minimum Number of Pushes to Type Word II

---
+++ 3016. Minimum Number of Pushes to Type Word II/leetcode_3016.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minimumPushes(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        counter = Counter(word)
+        counter = sorted(counter.items(), key=lambda x: -x[1])
+        buttons, cost, total = 8, 1, 0
+        for k,v in counter:
+            total += v*cost
+            buttons -= 1
+            if buttons == 0:
+                cost +=1
+                buttons = 8
+        return total
+                                                                                                                                                            
