5880c82f10ae59b9a1a4ef8cb0dc7f2259487c26
zsichina <zazasichina@gmail.com>
2024-08-10 15:09:36 +0400

    125. Valid Palindrome

---
+++ 0125. Valid Palindrome/leetcode_125.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        start, end = 0, len(s) - 1
+        while start < end:
+            while start < end and not s[start].isalnum():
+                start += 1
+            while start < end and not s[end].isalnum():
+                end -= 1
+            if s[start].lower() != s[end].lower():
+                return False
+            start += 1
+            end -= 1
+        return True
